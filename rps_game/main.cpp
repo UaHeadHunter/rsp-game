5f2419be611705e60f1e3b2a89763a3cf372bb31
HeadHunter <ua.head.hunterr@gmail.com>
2015-03-16 22:59:50 +0200

    game has been implemented (badly)

--- rps_game/main.cpp
+++ rps_game/main.cpp
@@ -20,6 +20,10 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctime>
 
 using namespace std;
 
@@ -40,6 +44,10 @@ const string cArgKeyVersion = "--version";
 const string cLevelStringEasy = "easy";
 const string cLevelStringNormal = "normal";
 const string cLevelStringHard = "hard";
+const string rock = "rock";
+const string scissors = "scissors";
+const string paper = "paper";
+
 const string cVersionNumer = "1.0.0";
 
 /*!
@@ -132,6 +140,7 @@ string stringArg(int argc, char* argv[], string argName)
 
     return result;
 }
+
 bool findVersion(int argc, char* argv[])
 {
 
@@ -143,7 +152,7 @@ bool findVersion(int argc, char* argv[])
     }
     return false;
 }
-bool ParseArgs (int argc, char* argv[])
+bool parseArgs (int argc, char* argv[])
 {
     int count = 1;
     for(int i = 1; i < argc; i++)
@@ -164,6 +173,85 @@ bool ParseArgs (int argc, char* argv[])
 
     return (argc == count)? false : true;
 }
+enum GameParams
+{
+    ERROR = -1,
+    ROCK = 0,
+    SCISSORS,
+    PAPER,
+    Length
+};
+void printResult(int result)
+{
+    if( result > 0)
+        if( result % 2 == 0)
+            cout << "You win" << endl;
+        else
+            cout << "CPU win" << endl;
+    else if( result < 0)
+        if( result % 2 != 0)
+            cout << "You win" << endl;
+        else
+            cout << "CPU win" << endl;
+    else if( result == 0)
+        if( result % 2 == 0)
+            cout << "TIE" << endl;
+}
+GameParams choiseFromString(string playerChoise)
+{
+    transform(playerChoise.begin(), playerChoise.end(), playerChoise.begin(), ::tolower);
+    if(rock == playerChoise)
+        return ROCK;
+    else if(scissors == playerChoise)
+        return SCISSORS;
+    else if(paper == playerChoise)
+        return PAPER;
+    else
+    {
+        if("exit" != playerChoise)
+            cout << "Bad input... Please, try again (rock | scissors | paper)" << endl << endl;
+        return ERROR;
+    }
+}
+int GencpuChoise(Level level)
+{
+    int cpuChoise;
+    if(level == EASY)
+    {
+        cpuChoise = 1;
+
+    }
+    else if(level == NORMAL)
+    {
+        cpuChoise = rand()% Length;
+
+    }
+    else
+    {
+        cout << "Sorry, but this level is not implemented yet" << endl;
+        return -1;
+    }
+    switch(cpuChoise)
+    {
+        case ROCK: cout << "CPU choose ROCK" << endl << endl; break;
+        case SCISSORS: cout << "CPU choose SCISSORS" << endl << endl; break;
+        case PAPER: cout << "CPU choose PAPER" << endl << endl; break;
+    }
+    return cpuChoise;
+}
+
+void gameEngine(GameParams pChoise, Level level)
+{
+    if(pChoise == ERROR)
+        return;
+
+    int cpuChoise = GencpuChoise(level);
+    if(cpuChoise == ERROR)
+        return;
+    int result = (pChoise - cpuChoise) % Length;
+    printResult(result);
+}
+
 
 /*!
  * \brief main
@@ -173,13 +261,14 @@ bool ParseArgs (int argc, char* argv[])
  */
 int main(int argc, char* argv[])
 {
+    srand (time(NULL));
     if (argc == 1)
     {
         showHelp();
         return 1;
     }
-
-    bool Permission = ParseArgs(argc, argv);
+    //catching bad parametrs
+    bool Permission = parseArgs(argc, argv);
     if(Permission)
     {
         cerr << "Wrong parametrs" << endl;
@@ -193,7 +282,7 @@ int main(int argc, char* argv[])
     // Parse level
     Level level = UNSPECIFIED;
     string levelStr = stringArg(argc, argv, cArgKeyLevel);
-
+    //I'm sure that it is clear to understanding
     if (!levelStr.empty())
     {
         level = parseLevel(levelStr);
@@ -205,6 +294,18 @@ int main(int argc, char* argv[])
         showHelp();
         return 1;
     }
+
+    string playerChoise;
+    int it = 0;
+    while(playerChoise != "exit" && it < 3)
+    {
+
+        cout << "Please, select your choise (rock | scissors | paper) or type exit to quit" << endl << endl;
+        cin >> playerChoise;
+        gameEngine(choiseFromString(playerChoise), level);
+        it++;
+    }
+    cout << "Game ended"<< endl;
     return 0;
 }
 
